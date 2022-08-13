18ec5bebb3a4a3d5a4b8a9bc489a7997a34024b7
victor-carancio <victor.carancio@gmail.com>
2022-08-13 17:55:11 -0400

    Se sube proyecto a repositorio

---
+++ app/js/main.js
@@ -0,0 +1,76 @@
+//toggle menu
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const headerContainer = document.querySelector('.header-container');
+const fadeElems = document.querySelector('.has-fade');
+const body = document.querySelector('.body');
+
+const home = document.getElementById('home-btn');
+const client= document.getElementById('client-btn');
+const contact = document.querySelectorAll('.contact-btn');
+
+//animations
+const contactSection = document.querySelector('.contact');
+const contactAnimLeft = document.querySelectorAll('.contact-anim-left');
+const contactAnimRight = document.querySelectorAll('.contact-anim-right');
+const AnimNameLeft = 'animate__fadeInLeft';
+const AnimNameRight = 'animate__fadeInRight';
+
+
+
+btnHamburger.addEventListener('click', () => {
+    
+    if(header.classList.contains('open')){
+
+        CloseHamburger();
+        
+    } else {
+        header.classList.add('open');
+        headerContainer.classList.add('menu-open');
+        fadeElems.classList.remove('fade-out');
+        fadeElems.classList.add('fade-in');
+        body.classList.add('no-scroll');
+        btnHamburger.classList.add('open');
+    }
+    
+
+});
+
+home.addEventListener('click', () => {
+    CloseHamburger();
+});
+client.addEventListener('click', () => {
+    CloseHamburger();
+});
+
+contact.forEach((element) => {
+    element.addEventListener('click', () => {
+        CloseHamburger();
+        contactSection.scrollIntoView();
+        window.scrollBy(0, -100);
+        contactAnimLeft.forEach((e) => {
+            e.classList.add(AnimNameLeft);
+        });
+        contactAnimRight.forEach((e) => {
+            e.classList.add(AnimNameRight);
+        });
+    });
+});
+
+
+
+
+function CloseHamburger(){
+    header.classList.remove('open');
+    headerContainer.classList.remove('menu-open');
+    fadeElems.classList.remove('fade-in');
+    fadeElems.classList.add('fade-out');
+    setTimeout(() => fadeElems.classList.remove('fade-out'), 500);
+    body.classList.remove('no-scroll');
+    
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    } else {
+        btnHamburger.classList.add('open');
+    }
+}
