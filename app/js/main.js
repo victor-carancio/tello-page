d07d6f98ef9f3ebce2985e71d7f97d2f9e43675b
victor-carancio <victor.carancio@gmail.com>
2022-08-15 14:16:02 -0400

    Se sube version final corregida

--- app/js/main.js
+++ app/js/main.js
@@ -1,51 +1,42 @@
-//toggle menu
-const btnHamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
-const headerContainer = document.querySelector('.header-container');
-const fadeElems = document.querySelector('.has-fade');
-const body = document.querySelector('.body');
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const headerContainer = document.querySelector(".header-container");
+const fadeElems = document.querySelector(".has-fade");
+const body = document.querySelector(".body");
 
-//btn
-const home = document.getElementById('home-btn');
-const client= document.getElementById('client-btn');
-const contact = document.querySelectorAll('.contact-btn');
-const project = document.querySelectorAll('.projects-btn');
-/* const projectSection = document.getElementById('projects'); */
+const home = document.getElementById("home-btn");
+const client = document.getElementById("client-btn");
+const contact = document.querySelectorAll(".contact-btn");
+const project = document.querySelectorAll(".projects-btn");
 
-//animations
-const contactSection = document.querySelector('.contact');
-const contactAnimLeft = document.querySelectorAll('.contact-anim-left');
-const contactAnimRight = document.querySelectorAll('.contact-anim-right');
-const AnimNameLeft = 'animate__fadeInLeft';
-const AnimNameRight = 'animate__fadeInRight';
+const contactSection = document.querySelector(".contact");
+const contactAnimLeft = document.querySelectorAll(".contact-anim-left");
+const contactAnimRight = document.querySelectorAll(".contact-anim-right");
+const AnimNameLeft = "animate__fadeInLeft";
+const AnimNameRight = "animate__fadeInRight";
 
-
-btnHamburger.addEventListener('click', () => {
-    
-    if(header.classList.contains('open')){
-
+btnHamburger.addEventListener("click", () => {
+    if (header.classList.contains("open")) {
         CloseHamburger();
-        
     } else {
-        header.classList.add('open');
-        headerContainer.classList.add('menu-open');
-        fadeElems.classList.remove('fade-out');
-        fadeElems.classList.add('fade-in');
-        body.classList.add('no-scroll');
-        btnHamburger.classList.add('open');
-    }    
-
+        header.classList.add("open");
+        headerContainer.classList.add("menu-open");
+        fadeElems.classList.remove("fade-out");
+        fadeElems.classList.add("fade-in");
+        body.classList.add("no-scroll");
+        btnHamburger.classList.add("open");
+    }
 });
 
-home.addEventListener('click', () => {
+home.addEventListener("click", () => {
     CloseHamburger();
 });
-client.addEventListener('click', () => {
+client.addEventListener("click", () => {
     CloseHamburger();
 });
 
 contact.forEach((element) => {
-    element.addEventListener('click', () => {
+    element.addEventListener("click", () => {
         CloseHamburger();
         contactSection.scrollIntoView();
         window.scrollBy(0, -100);
@@ -58,28 +49,17 @@ contact.forEach((element) => {
     });
 });
 
+function CloseHamburger() {
+    header.classList.remove("open");
+    headerContainer.classList.remove("menu-open");
+    fadeElems.classList.remove("fade-in");
+    fadeElems.classList.add("fade-out");
+    setTimeout(() => fadeElems.classList.remove("fade-out"), 500);
+    body.classList.remove("no-scroll");
 
-/* project.forEach((element) => {
-    element.addEventListener('click', () => {
-        CloseHamburger();
-        projectSection.scrollIntoView();
-        window.scrollBy(0, -100);
-    })
-}) */
-
-function CloseHamburger(){
-    header.classList.remove('open');
-    headerContainer.classList.remove('menu-open');
-    fadeElems.classList.remove('fade-in');
-    fadeElems.classList.add('fade-out');
-    setTimeout(() => fadeElems.classList.remove('fade-out'), 500);
-    body.classList.remove('no-scroll');
-    
-    if(btnHamburger.classList.contains('open')){
-        btnHamburger.classList.remove('open');
+    if (btnHamburger.classList.contains("open")) {
+        btnHamburger.classList.remove("open");
     } else {
-        btnHamburger.classList.add('open');
+        btnHamburger.classList.add("open");
     }
 }
-
-
